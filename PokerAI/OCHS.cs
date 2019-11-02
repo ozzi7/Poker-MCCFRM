d1e3c1f9b94b75b4918babdcee49d9846d34d582
ozzi7 <nielregen@gmail.com>
2019-11-02 12:45:15 +0100

    OCHS sampling parallelized

--- PokerAI/OCHS.cs
+++ PokerAI/OCHS.cs
@@ -18,7 +18,7 @@ namespace PokerAI
     /// so we have 2428287420 river combinations (7 cards), which need to be checked against the 169 opponent cards (monte carlo sampling)
     /// then we use k means with L2 distance metric to cluster all these "histograms" into 9000 or 5000 buckets
     /// 
-    /// only 169 player cards because even if it was an equivalent suit we dvicie the hand by its occurrence so it doesnt matter if we over or undersample
+    /// only 169 player cards because even if it was an equivalent suit we divide the hand by its occurrence so it doesnt matter if we over or undersample
     /// and the opponent hand: i dont think we can use the 169 cards here? it would make AhKh vs QhJh the same as vs QcJc but it is not the same  
     /// </summary>
     class OCHS
@@ -51,95 +51,97 @@ namespace PokerAI
 
             long deadCardMask = 0;
             histograms = new float[169, 8];
-            int[] cards = new int[2];
             long sharedLoopCounter = 0;
 
             using (var progress = new ProgressBar())
             {
-                for (int i = 0; i < 169; i++)
-                {
-                    for (int steps = 0; steps < nofMonteCarloSimulations; steps++)
-                    {
-                        indexer.unindex(indexer.rounds - 1, i, cards);
-                        deadCardMask |= (1L << cards[0]) + (1L << cards[1]);
+                Parallel.For(0, 169,
+                 i =>
+                 {
+                     for (int steps = 0; steps < nofMonteCarloSimulations; steps++)
+                     {
+                         int[] cards = new int[2];
+                         indexer.unindex(indexer.rounds - 1, i, cards);
+                         deadCardMask |= (1L << cards[0]) + (1L << cards[1]);
 
-                        int cardFlop1 = RandomGen.Next(0, 52);
-                        while (((1L << cardFlop1) & deadCardMask) != 0)
-                            cardFlop1 = RandomGen.Next(0, 52);
-                        deadCardMask |= (1L << cardFlop1);
+                         int cardFlop1 = RandomGen.Next(0, 52);
+                         while (((1L << cardFlop1) & deadCardMask) != 0)
+                             cardFlop1 = RandomGen.Next(0, 52);
+                         deadCardMask |= (1L << cardFlop1);
 
-                        int cardFlop2 = RandomGen.Next(0, 52);
-                        while (((1L << cardFlop2) & deadCardMask) != 0)
-                            cardFlop2 = RandomGen.Next(0, 52);
-                        deadCardMask |= (1L << cardFlop2);
+                         int cardFlop2 = RandomGen.Next(0, 52);
+                         while (((1L << cardFlop2) & deadCardMask) != 0)
+                             cardFlop2 = RandomGen.Next(0, 52);
+                         deadCardMask |= (1L << cardFlop2);
 
-                        int cardFlop3 = RandomGen.Next(0, 52);
-                        while (((1L << cardFlop3) & deadCardMask) != 0)
-                            cardFlop3 = RandomGen.Next(0, 52);
-                        deadCardMask |= (1L << cardFlop3);
+                         int cardFlop3 = RandomGen.Next(0, 52);
+                         while (((1L << cardFlop3) & deadCardMask) != 0)
+                             cardFlop3 = RandomGen.Next(0, 52);
+                         deadCardMask |= (1L << cardFlop3);
 
-                        int cardTurn = RandomGen.Next(0, 52);
-                        while (((1L << cardTurn) & deadCardMask) != 0)
-                            cardTurn = RandomGen.Next(0, 52);
-                        deadCardMask |= (1L << cardTurn);
+                         int cardTurn = RandomGen.Next(0, 52);
+                         while (((1L << cardTurn) & deadCardMask) != 0)
+                             cardTurn = RandomGen.Next(0, 52);
+                         deadCardMask |= (1L << cardTurn);
 
-                        int cardRiver = RandomGen.Next(0, 52);
-                        while (((1L << cardRiver) & deadCardMask) != 0)
-                            cardRiver = RandomGen.Next(0, 52);
-                        deadCardMask |= (1L << cardRiver);
+                         int cardRiver = RandomGen.Next(0, 52);
+                         while (((1L << cardRiver) & deadCardMask) != 0)
+                             cardRiver = RandomGen.Next(0, 52);
+                         deadCardMask |= (1L << cardRiver);
 
-                        int[] strength = new int[3];
-                        for (int card1Opponent = 0; card1Opponent < 51; card1Opponent++)
-                        {
-                            if (((1L << card1Opponent) & deadCardMask) != 0)
-                            {
-                                continue;
-                            }
-                            deadCardMask |= (1L << card1Opponent);
-                            for (int card2Opponent = card1Opponent + 1; card2Opponent < 52; card2Opponent++)
-                            {
-                                if (((1L << card2Opponent) & deadCardMask) != 0)
-                                {
-                                    continue;
-                                }
-                                ulong handSevenCards = (1uL << cards[0]) + (1uL << cards[1]) + (1uL << cardFlop1) + (1uL << cardFlop2) + (1uL << cardFlop3) + (1uL << cardTurn) + (1uL << cardRiver);
-                                ulong handOpponentSevenCards = (1uL << cardFlop1) + (1uL << cardFlop2) + (1uL << cardFlop3) + (1uL << cardTurn) + (1uL << cardRiver) + (1uL << card1Opponent) + (1uL << card2Opponent);
+                         int[] strength = new int[3];
+                         for (int card1Opponent = 0; card1Opponent < 51; card1Opponent++)
+                         {
+                             if (((1L << card1Opponent) & deadCardMask) != 0)
+                             {
+                                 continue;
+                             }
+                             deadCardMask |= (1L << card1Opponent);
+                             for (int card2Opponent = card1Opponent + 1; card2Opponent < 52; card2Opponent++)
+                             {
+                                 if (((1L << card2Opponent) & deadCardMask) != 0)
+                                 {
+                                     continue;
+                                 }
+                                 ulong handSevenCards = (1uL << cards[0]) + (1uL << cards[1]) + (1uL << cardFlop1) + (1uL << cardFlop2) + (1uL << cardFlop3) + (1uL << cardTurn) + (1uL << cardRiver);
+                                 ulong handOpponentSevenCards = (1uL << cardFlop1) + (1uL << cardFlop2) + (1uL << cardFlop3) + (1uL << cardTurn) + (1uL << cardRiver) + (1uL << card1Opponent) + (1uL << card2Opponent);
 
-                                int valueSevenCards = evaluator.Evaluate(handSevenCards);
-                                int valueOpponentSevenCards = evaluator.Evaluate(handOpponentSevenCards);
+                                 int valueSevenCards = evaluator.Evaluate(handSevenCards);
+                                 int valueOpponentSevenCards = evaluator.Evaluate(handOpponentSevenCards);
 
-                                int index = (valueSevenCards > valueOpponentSevenCards ? 0 : valueSevenCards == valueOpponentSevenCards ? 1 : 2);
+                                 int index = (valueSevenCards > valueOpponentSevenCards ? 0 : valueSevenCards == valueOpponentSevenCards ? 1 : 2);
 
-                                strength[index] += 1;
-                            }
-                        }
-                        float equity = (strength[0] + strength[1] / 2.0f) / (strength[0] + strength[1] + strength[2]);
-                        histograms[i, (Math.Min(7, (int)(equity * (float)nofOpponentClusters)))] += 1;
-                        deadCardMask = 0L;
+                                 strength[index] += 1;
+                             }
+                         }
+                         float equity = (strength[0] + strength[1] / 2.0f) / (strength[0] + strength[1] + strength[2]);
+                         histograms[i, (Math.Min(7, (int)(equity * (float)nofOpponentClusters)))] += 1;
+                         deadCardMask = 0L;
 
-                        Interlocked.Add(ref sharedLoopCounter, 1);
-                        progress.Report((double)Interlocked.Read(ref sharedLoopCounter) / (nofMonteCarloSimulations * 169));
-                    }
-                    float sum = 0.0f;
-                    for (int k = 0; k < nofOpponentClusters; ++k)
-                    {
-                        sum += histograms[i, k];
-                    }
-                    for (int k = 0; k < nofOpponentClusters; ++k)
-                    {
-                        histograms[i, k] /= sum;
-                    }
-                }
+                         Interlocked.Add(ref sharedLoopCounter, 1);
+                         progress.Report((double)Interlocked.Read(ref sharedLoopCounter) / (nofMonteCarloSimulations * 169));
+                     }
+                     float sum = 0.0f;
+                     for (int k = 0; k < nofOpponentClusters; ++k)
+                     {
+                         sum += histograms[i, k];
+                     }
+                     for (int k = 0; k < nofOpponentClusters; ++k)
+                     {
+                         histograms[i, k] /= sum;
+                     }
+                 });
             }
 
             Console.WriteLine("Calculated histograms: ");
+            int[] cardsOutput = new int[2];
             for (int i = 0; i < 169; ++i)
             {
-                cards = new int[2];
-                indexer.unindex(indexer.rounds - 1, i, cards);
+                cardsOutput = new int[2];
+                indexer.unindex(indexer.rounds - 1, i, cardsOutput);
                 SnapCall.Hand hand = new SnapCall.Hand();
-                hand.Cards.Add(new SnapCall.Card(cards[0]));
-                hand.Cards.Add(new SnapCall.Card(cards[1]));
+                hand.Cards.Add(new SnapCall.Card(cardsOutput[0]));
+                hand.Cards.Add(new SnapCall.Card(cardsOutput[1]));
                 hand.PrintColoredCards();
                 Console.Write(": ");
                 for (int j = 0; j < nofOpponentClusters; ++j)
@@ -159,7 +161,7 @@ namespace PokerAI
 
             for (int i = 0; i < 169; ++i)
             {
-                cards = new int[2];
+                cardsOutput = new int[2];
                 long index = indexer.indexLast(new int[] { startingHands[i].Cards[0].GetIndex(),
                     startingHands[i].Cards[1].GetIndex()});
                 Console.ForegroundColor = consoleColors[indices[index]];
